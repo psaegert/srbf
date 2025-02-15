46e9c43da49efc3c8f712e0dd8832dd46334ecd8
psaegert <paulsaegert@protonmail.com>
2025-02-15 13:04:11 +0100

    Improve pylint scdore

--- src/srbf/compat/nesymres.py
+++ src/srbf/compat/nesymres.py
@@ -1,8 +1,8 @@
-
-from functools import partial
 import json
-import omegaconf
+from functools import partial
 from typing import Callable
+
+import omegaconf
 
 from nesymres.architectures.model import Model
 from nesymres.dclasses import FitParams, BFGSParams
