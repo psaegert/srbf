553ba2d70ba06883288edcf9c06440c79e8c41b3
psaegert <paulsaegert@protonmail.com>
2025-10-16 23:25:35 +0200

    Set smoothing to 0 for evaluation progress bars

--- src/srbf/eval/evaluation.py
+++ src/srbf/eval/evaluation.py
@@ -190,7 +190,7 @@ class Evaluation():
                 preprocess=self.preprocess,
                 verbose=verbose,
                 batch_size=1,
-                tqdm_kwargs={'desc': 'Evaluating', 'total': size},
+                tqdm_kwargs={'desc': 'Evaluating', 'total': size, 'smoothing': 0.0},
                 tokenizer_oov='unk'  # Do not raise an error if an unknown token (operator) is encountered
             )
 
